de3c3009bd10388c98bfe509453ed21c39b0ab76
Agustin Alabarce <aj.alabarce@gmail.com>
2014-06-07 12:33:20 -0300

    Baja empresa

---
+++ FrbaCommerce/FrbaCommerce/Abm Empresa/Baja.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FrbaCommerce.Abm_Empresa
+{
+    public partial class Baja : Form
+    {
+        public Baja()
+        {
+            InitializeComponent();
+        }
+
+        private void Baja_Load(object sender, EventArgs e)
+{
+            this.empresaTableAdapter1.Fill(this.gD1C2014DataSet1.EMPRESA);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            DataSet nuevoDS = empresaTableAdapter1.FiltroEmpresa(gD1C2014DataSet1.EMPRESA, textBox1.Text, textBox2.Text, Convert.ToInt32(textBox3.Text));
+            this.empresaTableAdapter1.Fill(nuevoDS);
+        }
+    }
+}
---
+++ FrbaCommerce/FrbaCommerce/Abm Empresa/Baja.designer.cs
@@ -0,0 +1,184 @@
+﻿namespace FrbaCommerce.Abm_Empresa
+{
+    partial class Baja
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.empresaTableAdapter1 = new FrbaCommerce.GD1C2014DataSetTableAdapters.EMPRESATableAdapter();
+            this.gD1C2014DataSet1 = new FrbaCommerce.GD1C2014DataSet();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.eMPRAZONSOCIALDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.eMPMAILDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.eMPTELEFONODataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.eMPRESABindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.gD1C2014DataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eMPRESABindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(102, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Razón Social";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "CUIT";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(102, 77);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Email";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(102, 122);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            // 
+            // empresaTableAdapter1
+            // 
+            this.empresaTableAdapter1.ClearBeforeFill = true;
+            // 
+            // gD1C2014DataSet1
+            // 
+            this.gD1C2014DataSet1.DataSetName = "GD1C2014DataSet";
+            this.gD1C2014DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.eMPRAZONSOCIALDataGridViewTextBoxColumn,
+            this.eMPMAILDataGridViewTextBoxColumn,
+            this.eMPTELEFONODataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.eMPRESABindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(29, 164);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(539, 150);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // eMPRAZONSOCIALDataGridViewTextBoxColumn
+            // 
+            this.eMPRAZONSOCIALDataGridViewTextBoxColumn.DataPropertyName = "EMP_RAZON_SOCIAL";
+            this.eMPRAZONSOCIALDataGridViewTextBoxColumn.HeaderText = "Razón Social";
+            this.eMPRAZONSOCIALDataGridViewTextBoxColumn.Name = "eMPRAZONSOCIALDataGridViewTextBoxColumn";
+            // 
+            // eMPMAILDataGridViewTextBoxColumn
+            // 
+            this.eMPMAILDataGridViewTextBoxColumn.DataPropertyName = "EMP_MAIL";
+            this.eMPMAILDataGridViewTextBoxColumn.HeaderText = "Email";
+            this.eMPMAILDataGridViewTextBoxColumn.Name = "eMPMAILDataGridViewTextBoxColumn";
+            // 
+            // eMPTELEFONODataGridViewTextBoxColumn
+            // 
+            this.eMPTELEFONODataGridViewTextBoxColumn.DataPropertyName = "EMP_TELEFONO";
+            this.eMPTELEFONODataGridViewTextBoxColumn.HeaderText = "Teléfono";
+            this.eMPTELEFONODataGridViewTextBoxColumn.Name = "eMPTELEFONODataGridViewTextBoxColumn";
+            // 
+            // eMPRESABindingSource
+            // 
+            this.eMPRESABindingSource.DataMember = "EMPRESA";
+            this.eMPRESABindingSource.DataSource = this.gD1C2014DataSet1;
+            // 
+            // Baja
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 339);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Baja";
+            this.Text = "Baja";
+            this.Load += new System.EventHandler(this.Baja_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gD1C2014DataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.eMPRESABindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private FrbaCommerce.GD1C2014DataSetTableAdapters.EMPRESATableAdapter empresaTableAdapter1;
+        private GD1C2014DataSet gD1C2014DataSet1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn eMPRAZONSOCIALDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn eMPMAILDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn eMPTELEFONODataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource eMPRESABindingSource;
+    }
+}
