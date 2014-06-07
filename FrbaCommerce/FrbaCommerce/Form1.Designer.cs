0b521cf56a9e66af18b578fab2428993309116f2
meliguter <meliguter@gmail.com>
2014-06-07 01:47:12 -0300

    Arreglo un errorcito que quedó

--- FrbaCommerce/FrbaCommerce/Form1.Designer.cs
+++ FrbaCommerce/FrbaCommerce/Form1.Designer.cs
@@ -189,7 +189,6 @@
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Frba Commerce";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
