7e7d5ac7359da2d2651be6ae8213e16e335688c3
Ariel Vila <ariel_fv92@hotmail.com>
2014-06-08 23:26:45 -0300

    a

--- FrbaCommerce/FrbaCommerce/Abm Rol/ModRol.Designer.cs
+++ FrbaCommerce/FrbaCommerce/Abm Rol/ModRol.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // ModRol
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "ModRol";
+            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.Name = "ModRol";
+            this.Text = "Frba Commerce";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
