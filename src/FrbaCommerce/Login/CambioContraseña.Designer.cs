09235c9285c903f31845cac1b76f2c4bbdf10e25
Ariel Vila <ariel_fv92@hotmail.com>
2014-07-03 14:26:57 -0300

    Pongo claven encriptada y cambio un null en registrar

--- src/FrbaCommerce/Login/CambioContraseña.Designer.cs
+++ src/FrbaCommerce/Login/CambioContraseña.Designer.cs
@@ -53,6 +53,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(145, 20);
             this.textBox1.TabIndex = 1;
+            this.textBox1.UseSystemPasswordChar = true;
             // 
             // button1
             // 
